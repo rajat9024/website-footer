b6f0594bf9c745f5f7d4d782094bfc78125a4a5a
rajat9024 <8824rajat@gmail.com>
2024-06-07 13:14:06 +0530

    liet description

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,49 @@ function App() {
 
   return (
     <>
-     <h1>hello</h1>
+    <div className='mainfooter'>
+      <div>
+          <h2>Laxmi Devi Institute of Engineering & Technology</h2>
+          <br />
+          <p>Alwar-Tijara-Delhi Highway
+              Chikani, Alwar, Rajasthan
+              India –  301028</p>
+              <br />
+      <h3>+91-7073477274</h3>
+      <br />
+      <h3>+91-9829103101</h3>
+      <br />
+      <a href="">director@lietalwar.org</a>
+      
+      </div>
+      <div className='firstfooter'>
+      <h2>Quick Links</h2>
+      <br />
+      <a href="">about us</a>
+      <br />
+      <a href="">admission</a>
+      <br />
+      <a href="">Apply@</a><br />
+      <a href=''>Board of Management</a><br />
+      <a href=''>Grievance Redressal</a><br />
+      </div>
+      <div className='secondfooter'>
+      <h2>Courses</h2><br />
+      <a href="">B. Tech.</a><br />
+      <a href="">M. Tech.</a><br />
+      <a href="">MBA</a><br />
+      <a href="">Polytechnic</a><br />
+      </div>
+      <div className='thirdfooter'>
+        <h2>External Links</h2><br />
+        <a href="">AICTE</a><br />
+        <a href="">BTU</a><br />
+        <a href="">BTER</a><br />
+        <a href="">DELNET</a><br />
+
+      </div>
+    
+    </div>
     </>
   )
 }
